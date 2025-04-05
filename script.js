48b23d8f1af46c1ca0059cb2922566e451595542
namantigo <namantigo@gmail.com>
2025-04-05 14:50:14 +0300

    Добавлена автопрокрутка

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const items = document.querySelectorAll(".selectable");
-    items.forEach(item => {
-      item.addEventListener("click", () => {
-        item.classList.toggle("selected");
-      });
+  // Выделение .selectable
+  const items = document.querySelectorAll(".selectable");
+  items.forEach(item => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("selected");
     });
   });
-  
+
+  // Автопрокрутка вниз при выходе из полей ввода
+  const inputs = document.querySelectorAll("input, textarea");
+  inputs.forEach(el => {
+    el.addEventListener("blur", () => {
+      setTimeout(() => {
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: 'smooth'
+        });
+      }, 100);
+    });
+  });
+});
   
